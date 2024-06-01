955a5d005c80ff5528993cb9fe230386ba982774
domhankle <domhankle03@gmail.com>
2024-06-01 18:00:33 -0400

    Adding parsers

--- lua/dom/plugins/nvim-treesitter.lua
+++ lua/dom/plugins/nvim-treesitter.lua
@@ -1,3 +1,19 @@
 return {
-  'nvim-treesitter/nvim-treesitter'
+  'nvim-treesitter/nvim-treesitter',
+  config = function()
+    local configs = require'nvim-treesitter.configs'
+
+    configs.setup {
+      ensure_installed = {'c', 'python', 'typescript', 'html', 'scss', 'lua'},
+      sync_install = false,
+      auto_install = true,
+      highlight = {
+	enable = true,
+	additional_vim_regex_highlighting = false
+      },
+      ident = {
+	enable = true
+      }
+    }
+  end
 }
