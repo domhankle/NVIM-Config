fa11b1d584fd9d4afce1cf30e081b6a6f38b19f3
domhankle <domhankle03@gmail.com>
2024-06-01 22:23:43 -0400

    Adding lspconfig whatever that does

--- lua/dom/plugins/nvim-treesitter.lua
+++ lua/dom/plugins/nvim-treesitter.lua
@@ -1,19 +1,3 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  config = function()
-    local configs = require'nvim-treesitter.configs'
-
-    configs.setup {
-      ensure_installed = {'cpp'},
-      sync_install = false,
-      auto_install = true,
-      highlight = {
-	enable = true,
-	additional_vim_regex_highlighting = false
-      },
-      ident = {
-	enable = true
-      }
-    }
-  end
+  'nvim-treesitter/nvim-treesitter'
 }
