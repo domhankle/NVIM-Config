283b26883b117b4936ebea1214d78eeecf1c95ad
domhankle <domhankle03@gmail.com>
2024-06-02 23:10:19 -0400

    Working on the editor some more

--- lua/dom/lazy.lua
+++ lua/dom/lazy.lua
@@ -11,7 +11,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("dom.plugins", {
+require("lazy").setup({ { import = "dom.plugins", }, { import = "dom.plugins.lsp" } }, {
   checker = {
     enabled = true,
     notify = false,
